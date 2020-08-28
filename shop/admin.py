4a9c750ac23f88a9c1da50b0c7193b38a113e962
yeLyn36 <ghddnjf2901@gmail.com>
2020-08-28 15:50:11 +0900

    shop/admin CategoryAdmin, ProductAdmin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'price', 'stock', 'image', 'description',
+                    'available_display', 'available_order', 'created', 'updated']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
